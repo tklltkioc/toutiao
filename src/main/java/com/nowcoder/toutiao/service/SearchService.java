eb964b9b41f8552e4f3d2a789f3e282aa9b5eda7
tklltkioc <wusiyuong@gmail.com>
2019-09-25 21:23:02 +0800

    搜索注释修改

--- src/main/java/com/nowcoder/toutiao/service/SearchService.java
+++ src/main/java/com/nowcoder/toutiao/service/SearchService.java
@@ -19,48 +19,48 @@ import java.util.Map;
 @Service
 public class SearchService {
     private static final String SOLR_URL = "http://127.0.0.1:8983/solr/wenda";
-    private HttpSolrClient client = new HttpSolrClient.Builder(SOLR_URL).build();
+    private HttpSolrClient client = new HttpSolrClient.Builder (SOLR_URL).build ();
     private static final String QUESTION_TITLE_FIELD = "question_title";
     private static final String QUESTION_CONTENT_FIELD = "question_content";
 
-    public List<Question> searchQuestion(String keyword, int offset, int count,
-                                         String hlPre, String hlPos) throws Exception {
-        List<Question> questionList = new ArrayList<>();
-        SolrQuery query = new SolrQuery(keyword);
-        query.setRows(count);
-        query.setStart(offset);
-        query.setHighlight(true);
-        query.setHighlightSimplePre(hlPre);
-        query.setHighlightSimplePost(hlPos);
-        query.set("hl.fl", QUESTION_TITLE_FIELD + "," + QUESTION_CONTENT_FIELD);
-        QueryResponse response = client.query(query);
-        for (Map.Entry<String, Map<String, List<String>>> entry : response.getHighlighting().entrySet()) {
-            Question q = new Question();
-            q.setId(Integer.parseInt(entry.getKey()));
-            if (entry.getValue().containsKey(QUESTION_CONTENT_FIELD)) {
-                List<String> contentList = entry.getValue().get(QUESTION_CONTENT_FIELD);
-                if (contentList.size() > 0) {
-                    q.setContent(contentList.get(0));
+    public List<Question> searchQuestion (String keyword, int offset, int count,
+                                          String hlPre, String hlPos) throws Exception {
+        List<Question> questionList = new ArrayList<> ();
+        SolrQuery query = new SolrQuery (keyword);
+        query.setRows (count);
+        query.setStart (offset);
+        query.setHighlight (true);
+        query.setHighlightSimplePre (hlPre);
+        query.setHighlightSimplePost (hlPos);
+        query.set ("hl.fl", QUESTION_TITLE_FIELD + "," + QUESTION_CONTENT_FIELD);
+        QueryResponse response = client.query (query);
+        for (Map.Entry<String, Map<String, List<String>>> entry : response.getHighlighting ().entrySet ()) {
+            Question q = new Question ();
+            q.setId (Integer.parseInt (entry.getKey ()));
+            if (entry.getValue ().containsKey (QUESTION_CONTENT_FIELD)) {
+                List<String> contentList = entry.getValue ().get (QUESTION_CONTENT_FIELD);
+                if (contentList.size () > 0) {
+                    q.setContent (contentList.get (0));
                 }
             }
-            if (entry.getValue().containsKey(QUESTION_TITLE_FIELD)) {
-                List<String> titleList = entry.getValue().get(QUESTION_TITLE_FIELD);
-                if (titleList.size() > 0) {
-                    q.setTitle(titleList.get(0));
+            if (entry.getValue ().containsKey (QUESTION_TITLE_FIELD)) {
+                List<String> titleList = entry.getValue ().get (QUESTION_TITLE_FIELD);
+                if (titleList.size () > 0) {
+                    q.setTitle (titleList.get (0));
                 }
             }
-            questionList.add(q);
+            questionList.add (q);
         }
         return questionList;
     }
 
-    public boolean indexQuestion(int qid, String title, String content) throws Exception {
-        SolrInputDocument doc =  new SolrInputDocument();
-        doc.setField("id", qid);
-        doc.setField(QUESTION_TITLE_FIELD, title);
-        doc.setField(QUESTION_CONTENT_FIELD, content);
-        UpdateResponse response = client.add(doc, 1000);
-        //返回状态码为0则成功
-        return response != null && response.getStatus() == 0;
+    public boolean indexQuestion (int qid, String title, String content) throws Exception {
+        SolrInputDocument doc = new SolrInputDocument ();
+        doc.setField ("id", qid);
+        doc.setField (QUESTION_TITLE_FIELD, title);
+        doc.setField (QUESTION_CONTENT_FIELD, content);
+        UpdateResponse response = client.add (doc, 1000);
+        //  返回状态码为0则成功
+        return response != null && response.getStatus () == 0;
     }
 }
